0ce665311d7d9d381764f965f5220fc89860f66b
AshDest <ashuzamaheshe4@gmail.com>
2022-11-16 13:28:59 +0100

    other commit

--- GalerieSoft/GalerieSoft/Form1.Designer.cs
+++ GalerieSoft/GalerieSoft/Form1.Designer.cs
@@ -210,7 +210,9 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TERMINAL";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
