45a0de4102946a0ea954624f6220f14ec98f224b
AshDest <ashuzamaheshe4@gmail.com>
2022-11-16 14:38:58 +0100

    model commit

---
+++ GalerieSoft/GalerieSoft/Models/Paiement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalerieSoft.Models
+{
+    public class Paiement
+    {
+        public string Code { get; set; }
+        public DateTime DatePaie { get; set; }
+        public float MontantPaie { get; set; }
+    }
+}
---
+++ GalerieSoft/GalerieSoft/Models/Produit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalerieSoft.Models
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Designation { get; set; }
+        public int Typeproduit { get; set; }
+        public int Categorieproduit { get; set; }
+        public int Quantite { get; set; }
+        public float Prix { get; set; }
+    }
+}
